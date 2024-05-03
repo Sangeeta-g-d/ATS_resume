0d308eb26b34ba695b455d6ef7b8d6e6d5354807
vidyawandakar <vidyawandakar@gmail.com>
2024-05-03 13:38:28 +0530

    xdfcvgbhn

--- resume_builder/urls.py
+++ resume_builder/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('resumes',views.resumes,name="resumes"),
     path('resume_options',views.resume_options,name="resume_options"),
-    path('personal_info',views.personal_info,name="personal_info")
+    path('personal_info',views.personal_info,name="personal_info"),
+    path('work_history',views.work_history,name="work_history"),
+    path('education',views.education,name="education"),
     
 ]
